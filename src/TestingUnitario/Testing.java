01d964af28229858aa5ed93543fb8385e1989006
francofallatti <66625402+francofallatti@users.noreply.github.com>
2022-03-26 20:52:59 -0300

    practica test

--- src/TestingUnitario/Testing.java
+++ src/TestingUnitario/Testing.java
@@ -2,4 +2,69 @@ package TestingUnitario;
 
 public class Testing {
 
+	public static int sumarPositivos(int[] b) {
+		if (b == null) {
+			throw new NullPointerException(" El arreglo b no puede ser null!");
+		}
+		int suma = 0;
+		for (int i = 0; i < b.length; ++i) {
+			if (b[i] > 0) {
+				suma += b[i];
+			}
+		}
+		return suma;
+	}
+
+	public static boolean buscar(int[] b, int x) {
+		int i = 0;
+		while (i < b.length && b[i] != x) {
+			++i;
+		}
+		if (i < b.length) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static int maximo(int[] arreglo) {
+		if (arreglo == null || arreglo.length == 0) {
+			return 0;
+		}
+		int max = arreglo[0];
+		for (int i = 1; i < arreglo.length; ++i) {
+			max = Math.max(max, arreglo[i]);
+		}
+		return max;
+	}
+
+	// Escribir tests unitarios para el siguiente método, recordando que las
+	// excepciones que
+	// potencialmente lanza un método forman parte de su interfaz pública:
+	public int segundoMayor(int[] arreglo) {
+		if (arreglo.length < 2) {
+			throw new IllegalArgumentException(" Pocos elementos!");
+		}
+		int max = Math.max(arreglo[0], arreglo[1]);
+		int ret = Math.min(arreglo[0], arreglo[1]);
+		for (int i = 2; i < arreglo.length; ++i) {
+			if (arreglo[i] > max) {
+				ret = max;
+				max = arreglo[i];
+			} else if (arreglo[i] > ret) {
+				ret = arreglo[i];
+			}
+		}
+		return ret;
+	}
+
+	public static boolean ceroDuplicado(int[] a, int n) {
+		if (a == null)
+			throw new IllegalArgumentException();
+		int x = 0;
+		for (int i = 0; i < n; ++i)
+			if (a[i] == 0)
+				++x;
+		return x == 2;
+	}
 }
--- src/TestingUnitario/TestingTest.java
+++ src/TestingUnitario/TestingTest.java
@@ -7,13 +7,68 @@ import org.junit.Test;
 
 public class TestingTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
+	@Test(expected = NullPointerException.class)
+	public void sumarPositivosNullTest() {
+		Testing.sumarPositivos(null);
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void sumarPositivosVacioTest() {
+		int[] b= new int[0];
+		assertEquals(0, Testing.sumarPositivos(b));
+	}
+	@Test
+	public void sumarPositivosNegTest() {
+		int[] b= new int[] {-1,0,-4};
+		assertEquals(0, Testing.sumarPositivos(b));
+	}
+	@Test
+	public void sumarPositivosTest() {
+		int[] b= new int[] {1,2,3,4};
+		assertEquals(10, Testing.sumarPositivos(b));
+	}
+	@Test
+	public void sumarPositivosMezcladosTest() {
+		int[] b= new int[] {1,-2,-3,4};
+		assertEquals(5, Testing.sumarPositivos(b));
+	}
+	
+	
+	@Test
+	public void buscarTest() {
+		int[] b= new int[] {1,2,3,4};
+		assertTrue(Testing.buscar(b,3));
+	}
+	@Test
+	public void buscarFalseTest() {
+		int[] b= new int[] {1,2,3,4};
+		assertFalse(Testing.buscar(b,10));
+	}
+	
+	@Test 
+	public void maximoNullTest(){
+		assertEquals(0,Testing.maximo(null));
+	}
+	@Test
+	public void maximoZeroTest(){
+		int[]b=new int[] {0};
+		assertEquals(0,Testing.maximo(b));
+	}
+	@Test
+	public void maximoTest(){
+		int[] b= new int[] {1,2,442,43,4,865,323,13,4};
+		assertEquals(865,Testing.maximo(b));
+	}
+	@Test
+	public void maximoIgualesTest(){
+		int[] b= new int[] {1,1,1,1,1,1,1,1,1,1,1};
+		assertEquals(1,Testing.maximo(b));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void ceroDuplicadoExeptionTest() {
+		Testing.ceroDuplicado(null,0);
 	}
 
 }
