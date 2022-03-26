2a48b22e254c532e37c2df49b57b69bc60347b38
francofallatti <66625402+francofallatti@users.noreply.github.com>
2022-03-26 19:13:04 -0300

    first commit

---
+++ src/TestingUnitario/Testing.java
@@ -0,0 +1,5 @@
+package TestingUnitario;
+
+public class Testing {
+
+}
---
+++ src/TestingUnitario/TestingTest.java
@@ -0,0 +1,19 @@
+package TestingUnitario;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestingTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
